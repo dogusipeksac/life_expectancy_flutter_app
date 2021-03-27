996675ee68d3eb059bc82b9e590fe732eb69f832
dogus ipeksac <dogus.ads.1998@hotmail.com>
2021-03-27 23:24:43 +0300

    new  widgets learned

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -17,45 +19,50 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
       ),
       body: Column(
-          children: <Widget>[
-
-            Expanded(
-              flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: MyContainer(),
-                  ),  Expanded(
-                    flex: 1,
-                    child: MyContainer(),
-                  ),
-                ],
-              ),
-
-            ),  Expanded(
-              flex: 1,
-              child: MyContainer(),
-            ),Expanded(
-              flex: 1,
-              child: MyContainer(),
-            ),Expanded(
-              flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: MyContainer(renk: Colors.red),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: MyContainer(renk: Colors.yellow,),
-                  ),
-                ],
-              ),
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: MyContainer(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: MyContainer(),
+                ),
+              ],
             ),
-          ],
-
+          ),
+          Expanded(
+            flex: 1,
+            child: MyContainer(),
+          ),
+          Expanded(
+            flex: 1,
+            child: MyContainer(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: MyContainer(
+                    child: GenderFuct(icon: FontAwesomeIcons.venus,text: "KADIN",),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: MyContainer(
+                    child: GenderFuct(icon: FontAwesomeIcons.mars,text: "ERKEK",),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
 
       //  ),
@@ -63,19 +70,41 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class GenderFuct extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  GenderFuct({this.icon,this.text});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 40,
+          color: Colors.black54,),
+        SizedBox(height: 10,),
+        Text(
+         text,
+          style: TextStyle(fontSize: 20,color: Colors.black54,fontWeight:FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
 class MyContainer extends StatelessWidget {
   final Color renk;
-  MyContainer({this.renk:Colors.white});
+  final Widget child;
+  MyContainer({this.renk: Colors.white, this.child});
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      child: child,
       margin: EdgeInsets.all(10),
-
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: renk
-      ),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(10.0), color: renk),
     );
   }
 }
